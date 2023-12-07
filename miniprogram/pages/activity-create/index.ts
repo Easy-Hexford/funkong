9b333c98f77b401d61a1b166e611988f694ac343
sanfordsun <sanfordsun@tencent.com>
2023-12-07 20:28:06 +0800

    feat: activity

--- miniprogram/pages/activity-create/index.ts
+++ miniprogram/pages/activity-create/index.ts
@@ -1,23 +1,13 @@
 // pages/activity-create/index.ts
 Component({
-
-  /**
-   * 组件的属性列表
-   */
   properties: {
 
   },
 
-  /**
-   * 组件的初始数据
-   */
   data: {
     gridConfig: {column: 4, width: 72, height: 72}
   },
-
-  /**
-   * 组件的方法列表
-   */
+  
   methods: {
 
   }
