d04e1fa76fba6ce55eeb3c4d1e786b0ef4bd06d5
sanford <sanfordsun@tencent.com>
2023-12-15 15:44:49 +0800

    feat: home

--- miniprogram/pages/home/index.ts
+++ miniprogram/pages/home/index.ts
@@ -1,23 +1,89 @@
+import * as request from '../../services/index'
+import { IActivityInfo } from '../../services/index'
+
 Component({
+  options: {
+    pureDataPattern: /^_/
+  },
+
   properties: {
 
   },
-
   
   data: {
-    ActivityList: []
+    ActivityList: <Array<IActivityInfo>>[],
+    ActivityTotalCount: 0,
+
+    loading: true,
+    triggered: false,
+    _offset: 0,
+    _page: 20,
+    _freshing: false,
+    _loadMore: false,
+  },
+
+  lifetimes: {
+    attached() {
+      this.refreshActivityList()
+    }
   },
 
   pageLifetimes: {
     async show() { 
-      const tabComp = this.getTabBar()
-      tabComp.setData({
-        selected: 0
-      })
+      this.updateTabBar()
     }
   },
  
   methods: {
+    async refreshActivityList() {
+      request.getPulicActivityList({
+        Offset: 0,
+        Limit: this.data._page,
+      }).then(resp => {
+        this.setData({
+          loading: false,
+          ActivityList: resp.ActivityList,
+          ActivityTotalCount: resp.TotalCount,
+          _offset: resp.ActivityList.length
+        })
+      })
+    },
 
+    onRefresh() {
+      if (this.data._freshing) return
+      this.data._freshing = true
+      this.refreshActivityList()
+        .then(() => {
+          this.data._freshing = false
+          this.setData({
+            triggered: false,
+          })
+        })
+    },
+
+    loadMore() {
+      if (this.data._loadMore) return
+      this.data._loadMore = true
+      request.getPulicActivityList({
+        Offset: this.data._offset,
+        Limit: this.data._page,
+      }).then(resp => {
+        const ActivityList = this.data.ActivityList
+        ActivityList.push(...resp.ActivityList)
+        this.setData({
+          ActivityList,
+          ActivityTotalCount: resp.TotalCount,
+          _offset: ActivityList.length,
+          _loadMore: false
+        })
+      })
+    },
+
+    updateTabBar() {
+      const tabComp = this.getTabBar()
+      tabComp.setData({
+        selected: 0
+      })
+    },
   }
 })
