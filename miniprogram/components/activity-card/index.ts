13b788b7b6d90e79761539969a82d1e1fff8262c
sanford <sanfordsun@tencent.com>
2023-12-16 20:17:59 +0800

    fix: activity-card

--- miniprogram/components/activity-card/index.ts
+++ miniprogram/components/activity-card/index.ts
@@ -1,6 +1,7 @@
 import { formatActivityTime } from '../../utils/util'
 import { calcDistance, getLocation } from '../../utils/location'
 import { IActivityInfo } from '../../services'
+import { ActivitySignUpBlockTime } from '../../utils/constant'
 import dayjs from 'dayjs'
 
 const app = getApp()
@@ -21,18 +22,46 @@ Component({
   data: {
     distance: 0,
     beginTime: '',
-
-    auditStatus: ''
+    auditStatus: '',
+    canSignUp: false,
+    signUpText: '',
   },
 
   lifetimes: {
     attached() {
       this.formatDate()
       this.calcDistance()
+      this.getSignUpText()
     }
   },
 
   methods: {
+    getSignUpText() {
+      const Activity = this.data.activity as IActivityInfo
+      const diff = dayjs(Activity.BeginTime).unix() - dayjs().unix()
+      if (diff < ActivitySignUpBlockTime) {
+        this.setData({
+          canSignUp: false,
+          signUpText: '报名截止'
+        })
+      } else if (Activity.SignUpNum === Activity.ActivityRule.MaxSignUpNumber) {
+        this.setData({
+          canSignUp: false,
+          signUpText: '已满员'
+        })
+      } else if (Activity.SignUpNum === 0) {
+        this.setData({
+          canSignUp: true,
+          signUpText: '新活动'
+        })
+      } else {
+        this.setData({
+          canSignUp: true,
+          signUpText: `已有${Activity.SignUpNum}人`
+        })
+      }
+    },
+
     formatDate() {
       const BeginTime = this.data.activity.BeginTime
       this.setData({
@@ -40,10 +69,8 @@ Component({
       })
     },
 
-    
-
     calcDistance() {
-      const Activity = this.data.activity as IActivityInfo
+      const Activity = this.data.activity
       getLocation()
         .then(from => {
           const to = Activity.Location.Point
--- miniprogram/utils/constant.ts
+++ miniprogram/utils/constant.ts
@@ -1,2 +1,3 @@
 export const Day = 24 * 60 * 60
 export const Month = 30 * Day
+export const ActivitySignUpBlockTime = 3600
